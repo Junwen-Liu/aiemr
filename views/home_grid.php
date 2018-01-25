426c8b7943326e12fb04d1dc96719a7e3f157901
Liu <jliu5@HFHS.ORG>
2018-01-25 15:32:59 -0500

    add grid boxes

--- views/home_grid.php
+++ views/home_grid.php
@@ -1,24 +1,80 @@
-<div class="container-fluid">
+<div class="container-fluid mainContainer ">
   <div class="row">
-    <div class="col-3 col-sm-3 homebody">
-      <div class="homebox">
-          
-      </div>
-    </div>
-    <div class="col-3 col-sm-3 homebody">
-        <div class="homebox">
-            
+    <div class="col-sm-6 col-md-3 boxContainer">
+        <div class="subboxContainer">
+          <h4>Previous MDT</h4>
+             <div class="row justify-content-between">
+              <div class="col-sm-4"><p>staging</p></div>
+              <div class="col-sm-4"><p>CT3NXm1</p></div>
+              </div>
+            <p>treatment</p>
+            <p>1. <u>Active Survelliance</u></p>
+            <p>Comments</p>
+            <p>cT-T2b,based on the DRE. Only one positvie biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
+        </div>
+        <div class="subboxContainer">
+          <h4>Madical History</h4>
+             <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Symptonms</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Medications</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Previous surgeries</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Completed Chemotherapies</p></div>
+              <div class="col-sm-2"><p><u>No</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Completed radiation Therapies</p></div>
+              <div class="col-sm-2"><p><u>No</u></p></div>
+              </div>    
         </div>
     </div>
-    <div class="col-3 col-sm-3 homebody">
-      <div class="homebox">
-      
-      </div>
+    <div class="col-sm-6 col-md-3 boxContainer">
+        <div class="subboxContainer">
+          <h4>Leb tests</h4>
+             <div class="row justify-content-around">
+              <div class="col-sm-3">
+                  <p>PSA</p>
+                  <p><span class="definedfont1">7.7</span>ng/ml</p>
+              </div>
+              <div class="col-sm-3">
+                <p>Hb</p>
+                <p><span class="definedfont2">11.7</span>mmol/L</p>
+              </div>
+              <div class="col-sm-3">
+                 <p>Bilirubin</p>
+                 
+              </div>
+              </div>
+            <p>treatment</p>
+            <p>1. <u>Active Survelliance</u></p>
+            <p>Comments</p>
+            <p>cT-T2b,based on the DRE. Only one positvie biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
+        </div>
+        <div class="subboxContainer">
+          <h4>Previous MDT</h4>
+             <div class="row justify-content-between">
+              <div class="col-sm-4"><p>staging</p></div>
+              <div class="col-sm-4"><p>CT3NXm1</p></div>
+              </div>
+            <p>treatment</p>
+            <p>1. <u>Active Survelliance</u></p>
+            <p>Comments</p>
+            <p>cT-T2b,based on the DRE. Only one positvie biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
+        </div>
     </div>
-     <div class="col-3 col-sm-3 homebody">
-      <div class="homebox">
-          
-      </div>
+    <div class="col-sm-6 col-md-3">
+
+    </div>
+     <div class="col-sm-6 col-md-3">
+
     </div>
   </div>
 </div>
