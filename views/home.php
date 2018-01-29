a02f1a4d340688cbbee9959b478fffa50a4b95ad
Liu <jliu5@HFHS.ORG>
2018-01-29 15:40:03 -0500

    add progress bar

--- views/home.php
+++ views/home.php
@@ -1,5 +1,6 @@
 <?php
     include("home_search.php");
-    //include("home_timeline.php");
+    include("home_timeline.php");
+    include("home_progress.php");
     include("home_grid.php");
 ?>
--- views/home_timeline.php
+++ views/home_timeline.php
@@ -1,94 +1,35 @@
-<section class="cd-horizontal-timeline">
-	
-	<div class="events-content">
-		<ol>
-			<li class="selected" data-date="16/01/2014">
-				<h2>Horizontal Timeline</h2>
-				<em>January 16th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="28/02/2014">
-				<h2>Event title here</h2>
-				<em>February 28th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="20/04/2014">
-				<h2>Event title here</h2>
-				<em>March 20th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="20/05/2014">
-				<h2>Event title here</h2>
-				<em>May 20th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="09/07/2014">
-				<h2>Event title here</h2>
-				<em>July 9th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="30/08/2014">
-				<h2>Event title here</h2>
-				<em>August 30th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="15/09/2014">
-				<h2>Event title here</h2>
-				<em>September 15th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="01/11/2014">
-				<h2>Event title here</h2>
-				<em>November 1st, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="10/12/2014">
-				<h2>Event title here</h2>
-				<em>December 10th, 2014</em>
-				<p>	
-					Done
-				</p>
-			</li>
-
-			<li data-date="19/01/2015">
-				<h2>Event title here</h2>
-				<em>January 19th, 2015</em>
-				<p>	
-					Ongoing
-				</p>
-			</li>
-
-			<li data-date="03/03/2015">
-				<h2>Event title here</h2>
-				<em>March 3rd, 2015</em>
-				<p>	
-					Not yet
-				</p>
-			</li>
-		</ol>
-	</div> <!-- .events-content -->
+<section class="timeline">
+  <ol>
+    <li>
+      <div>
+        <time>2016</time>
+        Some content here
+      </div>
+    </li>
+     <li>
+      <div>
+        <time>2017</time>
+        Some content here
+      </div>
+    </li>
+     <li>
+      <div>
+        <time>2018</time>
+        Some content here
+      </div>
+    </li>
+      
+    <!-- more list items here -->
+     
+    <li></li>
+  </ol>
+   
+  <div class="arrows">
+    <button class="arrow arrow__prev disabled" disabled>
+      <img src="arrow_prev.svg" alt="prev timeline arrow">
+    </button>
+    <button class="arrow arrow__next">
+      <img src="arrow_next.svg" alt="next timeline arrow">
+    </button>
+  </div>
 </section>
