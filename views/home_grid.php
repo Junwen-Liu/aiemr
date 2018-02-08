012448ccea03dbefc114769b8907f93bd6d46605
Liu <jliu5@HFHS.ORG>
2018-02-08 16:59:09 -0500

    minor change on grid

--- views/home_grid.php
+++ views/home_grid.php
@@ -1,6 +1,6 @@
 <div class="container-fluid mainContainer ">
-  <div class="row">
-    <div class="col-sm-6 col-md-3 boxContainer">
+  <div class="row ">
+    <div class="col-sm-6 col-md-3 boxContainer ">
         <div class="subboxContainer">
           <h4>Previous MDT</h4>
              <div class="row justify-content-between">
@@ -10,7 +10,7 @@
             <p>treatment</p>
             <p>1. <u>Active Survelliance</u></p>
             <p>Comments</p>
-            <p>cT-T2b,based on the DRE. Only one positvie biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
+            <p>cT-T2b,based on the DRE. Only one positive biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
         </div>
         <div class="subboxContainer">
           <h4>Madical History</h4>
@@ -36,7 +36,7 @@
               </div>    
         </div>
     </div>
-    <div class="col-sm-6 col-md-3 boxContainer">
+    <div class="col-sm-6 col-md-3 boxContainer ">
         <div class="subboxContainer">
           <h4>Leb tests</h4>
              <div class="row justify-content-around">
@@ -53,11 +53,94 @@
                  
               </div>
               </div>
+        </div>
+        <div class="subboxContainer">
+          <h4>Comorbidities</h4>
+             <div class="row justify-content-around">
+              <div class="col-sm-6">
+                  <p>Cardiovascular disorders</p>
+                  <p>Thrombotic disorders</p>
+              </div>
+              <div class="col-sm-6">
+                <p>Vascular disorders</p>
+              </div>
+              </div>
+        </div>
+        <div class="subboxContainer">
+          <h4>Fitness for treatment</h4>
+             <div class="row justify-content-around">
+              <div class="col-sm-3">
+                  <p>BMI</p>
+                  <p><span class="definedfont1">23.5</span>kg/m</p>
+              </div>
+              <div class="col-sm-6">
+                <p>Patient's preferences</p>
+                <p><span class="definedfont2">Yes</span></p>
+              </div>
+              <div class="col-sm-3">
+                 <p>I-PSS</p>
+                 <p><span class="definedfont1">19</span></p>
+              </div>
+              </div>
+              <div class="row justify-content-around">
+              <div class="col-sm-3">
+                  <p>WHO</p>
+                  <p><span class="definedfont1">0</span></p>
+              </div>
+              <div class="col-sm-6">
+                <p>NCCN risk profile</p>
+                <p><span class="definedfont2"></span></p>
+              </div>
+              <div class="col-sm-3">
+              </div>
+              </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-3 boxContainer">
+        <div class="subboxContainer">
+        <div class="row justify-content-around">
+         <div class = "col-sm-8">
+             <h4>cTNM T3a Nx Mx <span class="definedfont2">vr 5</span></h4>
+         </div>
+         <div class="col-sm-2">
+            <p><span class="definedfont1">Edit</span></p>
+         </div>
+          
+            
+             <div class="row justify-content-between">
+              <div class="col-sm-4"><p>staging</p></div>
+              <div class="col-sm-4"><p>CT3NXm1</p></div>
+              </div>
             <p>treatment</p>
             <p>1. <u>Active Survelliance</u></p>
             <p>Comments</p>
-            <p>cT-T2b,based on the DRE. Only one positvie biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
+            <p>cT-T2b,based on the DRE. Only one positive biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
         </div>
+        <div class="subboxContainer">
+          <h4>Madical History</h4>
+             <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Symptonms</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Medications</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Previous surgeries</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Completed Chemotherapies</p></div>
+              <div class="col-sm-2"><p><u>No</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Completed radiation Therapies</p></div>
+              <div class="col-sm-2"><p><u>No</u></p></div>
+              </div>    
+        </div>
+    </div>
+    <div class="col-sm-6 col-md-3 boxContainer ">
         <div class="subboxContainer">
           <h4>Previous MDT</h4>
              <div class="row justify-content-between">
@@ -67,14 +150,31 @@
             <p>treatment</p>
             <p>1. <u>Active Survelliance</u></p>
             <p>Comments</p>
-            <p>cT-T2b,based on the DRE. Only one positvie biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
+            <p>cT-T2b,based on the DRE. Only one positive biopsy. Based on PSA-7 Ong/ml and Gleason...</p>
         </div>
-    </div>
-    <div class="col-sm-6 col-md-3">
-
-    </div>
-     <div class="col-sm-6 col-md-3">
-
+        <div class="subboxContainer">
+          <h4>Madical History</h4>
+             <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Symptonms</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Medications</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Previous surgeries</p></div>
+              <div class="col-sm-2"><p><u>Yes</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Completed Chemotherapies</p></div>
+              <div class="col-sm-2"><p><u>No</u></p></div>
+              </div>
+              <div class="row justify-content-between">
+              <div class="col-sm-10"><p>Completed radiation Therapies</p></div>
+              <div class="col-sm-2"><p><u>No</u></p></div>
+              </div>    
+        </div>
     </div>
   </div>
 </div>
